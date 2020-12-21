15d3565f0a761883960dfb0fbc6beb099d870e9f
Richard Chien <richardchienthebest@gmail.com>
2020-12-22 00:14:46 +0800

    move logic into Pressure struct

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,48 +1,48 @@
-use actix_web::{get, web, HttpResponse, Responder};
-use tera::Tera;
+// use actix_web::{get, web, HttpResponse, Responder};
+// use tera::Tera;
 
-lazy_static! {
-    pub static ref TEMPLATES: Tera =
-        Tera::new("templates/**/*.html").expect("Failed to parse templates.");
-}
+// lazy_static! {
+//     static ref TEMPLATES: Tera =
+//         Tera::new("templates/**/*.html").expect("Failed to parse templates.");
+// }
 
-#[get("/")]
-pub async fn index() -> impl Responder {
-    HttpResponse::Ok().body(
-        TEMPLATES
-            .render("index.html", &tera::Context::new())
-            .unwrap(),
-    )
-}
+// #[get("/")]
+// pub async fn index() -> impl Responder {
+//     HttpResponse::Ok().body(
+//         TEMPLATES
+//             .render("index.html", &tera::Context::new())
+//             .unwrap(),
+//     )
+// }
 
-#[get("/page/{page_num}/")]
-pub async fn index_page(web::Path(page_num): web::Path<u32>) -> impl Responder {
-    HttpResponse::Ok().body(format!("page_num: {}", page_num))
-}
+// #[get("/page/{page_num}/")]
+// pub async fn index_page(web::Path(page_num): web::Path<u32>) -> impl Responder {
+//     HttpResponse::Ok().body(format!("page_num: {}", page_num))
+// }
 
-#[get("/post/{year}/{month}/{day}/{name}/")]
-pub async fn post(
-    web::Path((year, month, day, name)): web::Path<(String, String, String, String)>,
-) -> impl Responder {
-    HttpResponse::Ok().body(format!("post: {}-{}-{}-{}", year, month, day, name))
-}
+// #[get("/post/{year}/{month}/{day}/{name}/")]
+// pub async fn post(
+//     web::Path((year, month, day, name)): web::Path<(String, String, String, String)>,
+// ) -> impl Responder {
+//     HttpResponse::Ok().body(format!("post: {}-{}-{}-{}", year, month, day, name))
+// }
 
-#[get("/archive/")]
-pub async fn archive() -> impl Responder {
-    HttpResponse::Ok().body("archive")
-}
+// #[get("/archive/")]
+// pub async fn archive() -> impl Responder {
+//     HttpResponse::Ok().body("archive")
+// }
 
-#[get("/category/{name}/")]
-pub async fn category(web::Path(name): web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("category: {}", name))
-}
+// #[get("/category/{name}/")]
+// pub async fn category(web::Path(name): web::Path<String>) -> impl Responder {
+//     HttpResponse::Ok().body(format!("category: {}", name))
+// }
 
-#[get("/tag/{name}/")]
-pub async fn tag(web::Path(name): web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("tag: {}", name))
-}
+// #[get("/tag/{name}/")]
+// pub async fn tag(web::Path(name): web::Path<String>) -> impl Responder {
+//     HttpResponse::Ok().body(format!("tag: {}", name))
+// }
 
-#[get("/404.html")]
-pub async fn page_not_found() -> impl Responder {
-    HttpResponse::Ok().body("404")
-}
+// #[get("/404.html")]
+// pub async fn page_not_found() -> impl Responder {
+//     HttpResponse::Ok().body("404")
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use pressure::Pressure;
+
 fn main() -> std::io::Result<()> {
-    pressure::serve("127.0.0.1", 8080)
+    let pressure = Pressure::new(
+        std::env::var("INSTANCE_PATH").unwrap_or(
+            std::env::current_dir()
+                .unwrap()
+                .into_os_string()
+                .into_string()
+                .unwrap(),
+        ),
+    );
+    pressure.serve("127.0.0.1", 8080)
 }
