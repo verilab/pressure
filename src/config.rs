559a01af0b868c8953cf86d8eed8a985446bc644
Richard Chien <richardchienthebest@gmail.com>
2020-12-31 14:28:01 +0800

    refactor config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
 
 use crate::{Instance, Result};
@@ -22,9 +24,15 @@ pub struct WebConfig {
     pub posts_per_page_on_index: u32,
 }
 
-pub fn load_config(instance: &Instance) -> Result<Config> {
-    let config_str = std::fs::read_to_string(instance.root_folder.join("config.toml"))?;
-    Ok(toml::from_str(&config_str)?)
+impl Config {
+    pub fn from_str(config_str: &str) -> Result<Config> {
+        Ok(toml::from_str(&config_str)?)
+    }
+
+    pub fn load<T: Into<PathBuf>>(config_path: T) -> Result<Config> {
+        let config_str = std::fs::read_to_string(config_path.into())?;
+        Self::from_str(&config_str)
+    }
 }
 
 #[cfg(test)]
@@ -32,10 +40,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_config_from_str() {
+        let config_str = "
+[site]
+title = \"BLOG\"
+subtitle = \"SOME BLOG.\"
+author = \"NAME\"
+timezone = \"Asia/Shanghai\"
+
+[web]
+posts_per_page_on_index = 10
+        ";
+        let config = Config::from_str(config_str).unwrap();
+        assert_eq!(config.site.title, "BLOG");
+        assert_eq!(config.site.author.unwrap(), "NAME");
+        assert_eq!(config.web.posts_per_page_on_index, 10);
+    }
+
+    #[test]
     fn test_load_config() {
-        let instance = Instance::new("tests/test_inst");
-        println!("{}", instance.root_folder.to_str().unwrap());
-        let config = load_config(&instance).unwrap();
+        let config = Config::load("tests/test_inst/config.toml").unwrap();
         assert_eq!(config.site.title, "My Blog");
         assert!(config.site.subtitle.is_some());
         assert_eq!(config.site.subtitle.unwrap(), "Here is my blog.");
